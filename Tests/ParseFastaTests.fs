c6212758037ae4a182980955b5a3df79b8178183
Mitch Nenn <mitch.nenn@earthlink.net>
2021-07-05 11:59:25 -0700

    Added a unit test for parsing a single Fasta entry.

--- Tests/ParseFastaTests.fs
+++ Tests/ParseFastaTests.fs
@@ -3,7 +3,6 @@ module ParseFastaTests
 open System.Text
 open System.IO
 open ParseFasta
-open FParsec
 open Xunit
 open Xunit.Abstractions
 open FsUnit
@@ -11,7 +10,7 @@ open FsUnit
 type ``Parse Fasta file tests`` (output:ITestOutputHelper) =
     
     [<Fact>]
-    member ths.``Read raw entries test`` () =
+    member this.``Read raw entries test`` () =
         // Arrange.
         let path = Path.Combine(__SOURCE_DIRECTORY__, "Data", "data.fasta")
         // Act.
@@ -19,6 +18,35 @@ type ``Parse Fasta file tests`` (output:ITestOutputHelper) =
         // Assert
         entries |> List.iter (fun i -> output.WriteLine(i))
         entries |> List.length |> should equal 9
+
+    [<Fact>]
+    member this.``Parse Fasta entry test.`` () =
+        // Arrange.
+        let entry = new StringBuilder()
+        entry.AppendLine(">Rosalind_7067") |> ignore
+        entry.AppendLine("GCCGGGTGCCGTGGCAAGAGTAGCGGTTCTCAGACCATAGTCGCTTCCATCAAGAGGGGG") |> ignore
+        entry.AppendLine("TCACGGCGGACGACTGCTTCGGTATAGCAGGCGGTCATTATTCGTTCGTATAATGGATAT") |> ignore
+        entry.AppendLine("CTGTCAGTCTTCTAGACGAGTTACAACCGTAGAGGTTGTTCTTAGGTGGATCGCAGCGTA") |> ignore
+        entry.AppendLine("CCGCACTTGCCGCTTTGGACAAGTGCTACTGTTCAACACTCCTACTCGTGTGTCTGGGAT") |> ignore
+        entry.AppendLine("CCCGAATAAATACCACAGAGGTATCCCCCTCCGATTTGATCATGGAACCACCCTCCGCCA") |> ignore
+        entry.AppendLine("GATCTCCCAGAGAGCGGGGTAGTCGGGTTTTATATGTTATACAGTCTATGGAACAATTCT") |> ignore
+        entry.AppendLine("TTTCACCCCATCAATAATCGCACCCATAAATGGTTATTGGTTCTCGTACTCCCTACATAA") |> ignore
+        entry.AppendLine("CCTGCAAGTCGCTCCCATTGTTACACCTGCGTAACAGAATTCCATAGTGTAGGTTTAGCT") |> ignore
+        entry.AppendLine("GCTGTGGGCCCTCTAGCCTTGTCCTTAGGTCTCACTGCTAAAGCACCGAAATTAACGTAA") |> ignore
+        entry.AppendLine("GGCTTTAAGTAAGAGTTCCTTGCAGAGAATTTAAGAAGCGAACCCGCCGCTTTTCATCCA") |> ignore
+        entry.AppendLine("CGTTGAGGCCAGCTCCACTCTTTTGAACAGACAGTCGCGAAGGACACCAGGGGTAACCTA") |> ignore
+        entry.AppendLine("TTAGACCTCAACCGGTCATAGCTTGGACCAGAGACGGCGGACGCGGAACCTCAGTGATAG") |> ignore
+        entry.AppendLine("GATGCACTGAACCAAATCGCCGTCCTCGCATCGCAAGACAAAGCCCAGTGTTACTGTACG") |> ignore
+        entry.AppendLine("CACAGATAGCATACGATCTCCTTCCGCCAGGAGGGGAGTGATAAGACCTACAGTGGTTCT") |> ignore
+        entry.AppendLine("AAGTACCGCACGATGCGGGGAGGCGACCCATGATGTCAGAAGCGATACATGCACCGTCCC") |> ignore
+        entry.AppendLine("CAGACACGTATAGCTGCGATTCTCTATAAGTGGCCCCCAGCCTTCAACAGTTAGCAATCA") |> ignore
+        entry.AppendLine("TTTCGCCGTAGCCGCAGCGAA") |> ignore
+        // Act.
+        let fasta = parseEntry (entry.ToString())
+        // Assert.
+        fasta.Description |> should equal "Rosalind_7067"
+        fasta.Sequence |> should startWith "GCCGGGTGCCGTGGCAAGAGTAGCGGTTCTCAGACCATAGTCGCTTCCATCAAGAGGGGGTC"
+        fasta.Sequence |> should endWith "ATCATTTCGCCGTAGCCGCAGCGAA"
 
     [<Fact>]
     member this.``Parse file happy path`` () =
